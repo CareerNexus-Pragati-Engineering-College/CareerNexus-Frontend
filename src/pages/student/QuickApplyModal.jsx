7b0e060b361ed373db0a4ca94f1a4ef51cedb292
Pranay Kandikonda <pranayk74174174@gmail.com>
2025-07-07 17:59:17 +0530

    saradaaga apply form lo changes cheydam jarigindhi - itluu mee guna bhai

--- src/pages/student/QuickApplyModal.jsx
+++ src/pages/student/QuickApplyModal.jsx
@@ -1,10 +1,9 @@
 // src/components/QuickApplyModal.jsx
 import React, { useState, useEffect } from "react";
 import { FaTimes } from "react-icons/fa";
-import requestApi from "../../services/request";
-import getUserId from "../../services/getUserId";
+import axios from "axios";
 
-const QuickApplyModal = ({ job, onClose, onApply }) => {
+const QuickApplyModal = ({ job, onClose, onApply, userId }) => {
   const [formData, setFormData] = useState({
     FirstName: "",
     LastName: "",
@@ -18,20 +17,11 @@ const QuickApplyModal = ({ job, onClose, onApply }) => {
     graduationYear: "",
     resume: null,
   });
-  const parseArray = (arr) => {
-  if (Array.isArray(arr)) return loc;
-  try {
-    return JSON.parse(arr);
-  } catch {
-    return [arr]; 
-  }
-};
 
   const [originalData, setOriginalData] = useState({});
   const [errors, setErrors] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [isModified, setIsModified] = useState(false);
-  const userId=getUserId();
 
   useEffect(() => {
     const fetchStudentData = async () => {
@@ -42,22 +32,13 @@ const QuickApplyModal = ({ job, onClose, onApply }) => {
         const populated = {
           FirstName: data.FirstName || "",
           LastName: data.LastName || "",
-        const res = await requestApi.get(
-          `/student/${userId}/profile`
-        );
-        const data = res.data;
-
-        setFormData((prev) => ({
-          ...prev,
-          FirstName: data.firstName || "",
-          LastName: data.lastName || "",
           userId: data.userId || "",
-          Department: data.department || "",
-          Year: data.year || "",
-          Cgpa: data.cgpa || "",
-          Email: data.email || "",
-          Phone: data.phone || "",
-          skills: parseArray(data?.skills)  || "",
+          Department: data.Department || "",
+          Year: data.Year || "",
+          Cgpa: data.Cgpa || "",
+          Email: data.Email || "",
+          Phone: data.Phone || "",
+          skills: data.skills?.join(", ") || "",
           graduationYear: data.graduationYear || "",
           resume: null,
         };
