59dff25ec9a6bbc9867d3ed6e042855782f2fe2b
Pranay_kandikonda <pranayk74174174@gmail.com>
2026-03-28 23:57:14 +0530

    code readability increased for Recruiter.jsx

--- src/pages/recruiter/Recruiter.jsx
+++ src/pages/recruiter/Recruiter.jsx
@@ -251,7 +251,9 @@ const Recruiter = () => {
                 {/* 📍 Navbar */}
       <NavbarRecruiter />
 
-      {/* 📍 Footer */}
+                
+                
+                {/* 📍 Footer */}
       <footer className="bg-gradient-to-r from-[#130c22] via-[#1b1435] to-[#0f0c1d] border-t border-violet-500/20 py-8 mt-10">
         <div className="container mx-auto px-4 sm:px-8 lg:px-16 text-center text-[#EADFFD] space-y-2">
           <p className="text-sm sm:text-base opacity-90">
